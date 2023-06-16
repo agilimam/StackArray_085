01203077185e96e4a74f3fa52dc0f24a21ab6d2b
agilimam <agilimamalhafizi04@gmail.com>
2023-06-16 16:14:13 +0700

    membuat pengulangan untuk menghapus pop

--- StackAlgoritma/StackAlgoritma.cpp
+++ StackAlgoritma/StackAlgoritma.cpp
@@ -14,4 +14,29 @@ public:
 		top = -1;
 	}
 
+	void push() {
+		int element;
+		cout << "Enter element : ";
+		cin >> element;
+
+		if (top == 4) {
+			cout << "\nNumber of data exceed the limit" << endl; //step 1
+			return ;
+		}
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " ditambahkan(pushed)" << endl;
+	}
+
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop." << endl; //1.a
+			return; //1.b
+		}
+
+		cout << "\nThe popped element is : " << stack_array[top] << endl; //step 2
+		top--; //step 3
+	}
+
 	
